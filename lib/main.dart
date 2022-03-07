1f7ca58f907ef1d87760b8d2172d5b885fadad9a
JoaoPaulo02438 <joao.paulo02438@gmail.com>
2022-03-07 12:59:52 -0300

    agenda contatos

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+// @dart=2.9
+import 'package:agenda_contatos/ul/contact_page.dart';
+import 'package:agenda_contatos/ul/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
